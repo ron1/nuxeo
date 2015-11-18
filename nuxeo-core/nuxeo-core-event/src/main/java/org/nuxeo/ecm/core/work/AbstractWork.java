2adef419661265666a21b72b8c0a8319dd755d48
Thomas Roger <troger@nuxeo.com>
2015-11-18 14:33:18 +0100

    NXP-18000: add back Work#getUserId method for backward compatibility

--- nuxeo-core/nuxeo-core-event/src/main/java/org/nuxeo/ecm/core/work/AbstractWork.java
+++ nuxeo-core/nuxeo-core-event/src/main/java/org/nuxeo/ecm/core/work/AbstractWork.java
@@ -478,6 +478,15 @@ public abstract class AbstractWork implements Work {
         return originatingUsername;
     }
 
+    /**
+     * @deprecated since 7.10. Use {@link #getOriginatingUsername()} instead.
+     */
+    @Override
+    @Deprecated
+    public String getUserId() {
+        return getOriginatingUsername();
+    }
+
     @Override
     public long getSchedulingTime() {
         return schedulingTime;
